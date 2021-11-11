0f032d538cd635a86006ed0be137b850a36b36ac
Syrgakcompany <92498012+Syrgakcompany@users.noreply.github.com>
2021-11-11 09:51:47 +0600

    Update valid(1).js

--- valid(1).js
+++ valid(1).js
@@ -25,14 +25,14 @@ function validated(){
 
 }
 function email_Verify(){
-	if (email.value.length >= 8) {
+	if (email.value.length >= sbabanov@gmail.com) {
 		email.style.border = "1px solid silver";
 		email_error.style.display = "none";
 		return true;
 	}
 }
 function pass_Verify(){
-	if (password.value.length >= 5) {
+	if (password.value.length >= syrgak) {
 		password.style.border = "1px solid silver";
 		pass_error.style.display = "none";
 		return true;
